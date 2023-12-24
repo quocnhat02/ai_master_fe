3713c37529d2b0bb3a8af67b15bad5d7005f4391
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-12-24 21:02:39 +0700

    login

--- my-app/app/(root)/layout.tsx
+++ my-app/app/(root)/layout.tsx
@@ -1,19 +1,7 @@
-import Link from 'next/link';
-import React, { useState } from 'react';
+import React from 'react';
 
 const Page = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='flex'>
-      <div className='flex gap-4 p-5 flex-col'>
-        <div className='flex gap-4'>
-          <Link href={'/'}>Home</Link>
-          <Link href={'/test1'}>Test 1</Link>
-        </div>
-
-        <div className=''>{children}</div>
-      </div>
-    </div>
-  );
+  return <>{children}</>;
 };
 
 export default Page;
