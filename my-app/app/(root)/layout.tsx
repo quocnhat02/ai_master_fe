29bc891e6dbe2ef16d15156e6c5f7b058bdaf309
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-04 17:22:08 +0700

    delete all

--- my-app/app/(root)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Page = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
-};
-
-export default Page;
--- my-app/app/(root)/page.tsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <div className=''>Home</div>;
-};
-
-export default Page;
